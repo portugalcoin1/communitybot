4857cd6d77032871b38e6c95bcc3e11d9aa8b309
Miguel Rocha <miguel@juntadigital.pt>
2018-11-07 00:43:22 +0000

    fix

--- communitybot.js
+++ communitybot.js
@@ -218,8 +218,9 @@ function sendVote(name, post, retries) {
        utils.log('sp_bot: ' + sp_bot);
 			});
 
-    var vote_weight_number = (sp_value_member / sp_total_bot)  * 10000;
-    config.vote_weight =  parseInt(vote_weight_number);
+    var vote_weight_number = (sp_value_member / sp_total_bot) * 10000;
+    utils.log('vote_weight_number: ' + vote_weight_number);
+    config.vote_weight =  Number(vote_weight_number);
     //config.vote_weight = 200;
     utils.log('Member vote weight: ' + config.vote_weight);
   }else{
