ea64f2395de4105bfb6d23682863dad4e9c42ac1
Miguel Rocha <miguel@juntadigital.pt>
2018-11-07 23:20:04 +0000

    Remove Cast

--- communitybot.js
+++ communitybot.js
@@ -219,9 +219,9 @@ function sendVote(name, post, retries) {
 			});
 
     var vote_weight_number = (sp_value_member / sp_bot) * 10000;
-    utils.log('vote_weight_number: ' + parseInt(vote_weight_number);
+    utils.log('vote_weight_number: ' + vote_weight_number;
 
-    config.vote_weight =  parseInt(vote_weight_number);
+    config.vote_weight =  vote_weight_number;
     utils.log('Member vote weight: ' + config.vote_weight);
   }else{
     config.vote_weight = 100;
