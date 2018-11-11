2dbf66f24f397be13afbf8bc175221106083558b
Miguel Rocha <miguel@juntadigital.pt>
2018-11-11 23:40:03 +0000

    Logs dates

--- communitybot.js
+++ communitybot.js
@@ -168,8 +168,12 @@ function voteNext() {
         // Make sure the post is less than 1 days old
         if((new Date(AddMinutesToDate(new Date(post.created + 'Z'),15)+ 'Z')) < new Date()) {
           utils.log('*** I can vote');
+          utils.log('*** Add minutes ->' + new Date(AddMinutesToDate(new Date(post.created + 'Z'),15)+ 'Z'));
+          utils.log('*** Date today ->' + new Date());
         }else{
           utils.log('*** I can´t vote');
+          utils.log('*** Add minutes ->' + new Date(AddMinutesToDate(new Date(post.created + 'Z'),15)+ 'Z'));
+          utils.log('*** Date today ->' + new Date());
           continue;
         }
 
